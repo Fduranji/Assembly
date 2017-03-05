7fbc7b20343d16ef70c398af3a7ce4c67277e80b
Francisco Duran <Franciscoduran93@gmail.com>
2017-03-05 16:11:37 -0700

    Update FranciscoDuranHW4CS2400.s

--- FranciscoDuranHW4CS2400.s
+++ FranciscoDuranHW4CS2400.s
@@ -1,35 +1,35 @@
-;****************************************************
-; Programmer: Francisco Duran Jimenez  				*
-; Date: March 4, 2017			   					*
-; File: FranciscoDuranHW4CS2400.s					*
+;********************************************************
+; Programmer: Francisco Duran Jimenez  			*
+; Date: March 4, 2017			   		*
+; File: FranciscoDuranHW4CS2400.s			*
 ; Description: IEEE floating point number to		*
 ;	TNS floating point number converter, and vise	*
 ;	versa, a comparison of the converted numbers	*
 ;	to the originals is also done, to check for 	*
-;	accuracy. 										*
-;	r0 == Original IEEE, r1 == Original TNS			*
-;	r9 == Converted IEEE, r8 == Converted TNS		*
-;	Comparison between r0 and r9 then r1 and r8		*
+;	accuracy. 					*
+;	r0 == Original IEEE, r1 == Original TNS		*
+;	r9 == Converted IEEE, r8 == Converted TNS	*
+;	Comparison between r0 and r9 then r1 and r8	*
 ;	If both numbers are correct, r10 and r11 will	*
-;	hold the values of 1, 0 if otherwise			*
-;****************************************************
+;	hold the values of 1, 0 if otherwise		*
+;********************************************************
 	AREA	FranciscoDuranHW4CS2400, CODE, READONLY
 	ENTRY
 	
 start
-	LDR r0, IEEE	; loads IEEE FP number 436B1000 into r0
+	LDR r0, IEEE		; loads IEEE FP number 436B1000 into r0
 	LDR r1, TNS		; loads TNS FP number 6B100107 into r1
-	BL IEEEtoTNS	; subroutine to convert IEEE into TNS
-					; converted number stored in r8
+	BL IEEEtoTNS		; subroutine to convert IEEE into TNS
+				; converted number stored in r8
 	CMP r1, r8		; comparing r1(original TNS) to r8(converted TNS), should be equal
-	MOVEQ r10, #1	; if they equal, move 1 into r10
-	MOVNE r10, #0	; if they are not equal, move 0 into r10
-	BL TNStoIEEE	; subroutine to convert TNS into IEEE
-					; converted number stored in r9
+	MOVEQ r10, #1		; if they equal, move 1 into r10
+	MOVNE r10, #0		; if they are not equal, move 0 into r10
+	BL TNStoIEEE		; subroutine to convert TNS into IEEE
+				; converted number stored in r9
 	CMP r0, r9		; comparing r0(original IEEE) to r9(converted IEEE), should be equal
-	MOVEQ r11, #1	; if they equal, move 1 into r11
-	MOVNE r11, #0 	; if they are not equal, move 0 into r11	
-	B	.			; terminate
+	MOVEQ r11, #1		; if they equal, move 1 into r11
+	MOVNE r11, #0 		; if they are not equal, move 0 into r11	
+	B	.		; terminate
 
 IEEEtoTNS
 	;masks used for IEEE to TNS conversion
@@ -47,8 +47,8 @@ IEEEtoTNS
 	MOV r7, r7, LSL #9	; shifts significand 9 bits to the right for TNS
 	ORR r8, r5, r6		; pack sign and exponent
 	ORR r8, r8, r7		; pack significand with sign and exponent into r8
-						; r8 now holds the converted number (TNS)
-	BX	r14				; return
+				; r8 now holds the converted number (TNS)
+	BX	r14		; return
 
 TNStoIEEE
 	;masks used for TNS to IEEE conversion
@@ -63,17 +63,17 @@ TNStoIEEE
 	MOV r6, r6, LSL #23	; shifts exponent 23 bits to the left for IEEE
 	AND r7, r1, r4		; unpack significand, store in r7
 	MOV r7, r7, LSR #8	; shifts significand 8 bits to the right, 
-						; loosing 1 bit precision at the LSB
+				; loosing 1 bit precision at the LSB
 	ORR r9, r5, r6		; pack sign and exponent
 	ORR r9, r9, r7		; pack significand with sign and exponent into r9
-						; r9 now holds the converted number (IEEE)
-	BX r14				; return
+				; r9 now holds the converted number (IEEE)
+	BX r14			; return
 
 
 IEEE	DCD	&436B1000	; IEEE floating point number. In decimal = 235.0625
-						; 0|10000110|11010110001000000000000| in binary IEEE format
+				; 0|10000110|11010110001000000000000| in binary IEEE format
 TNS		DCD &6B100107	; TNS floating point number. In decimal = 235.0625
-						; 0|1101011000100000000000|100000111| in binary TNS format
+				; 0|1101011000100000000000|100000111| in binary TNS format
 
 SignMask	DCD &80000000	; Mask for sign bit, same for IEEE and TNS
 IEEEExMask	DCD &7F800000	; Mask for IEEE exponent
